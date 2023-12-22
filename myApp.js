3962a97bc0747f59b6b1a4e817b228312c19af4a
Villegas, Edgar <Edgar.Villegas@mckesson.com>
2023-12-22 09:50:46 -0600

    Add model person

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
+const PersonModel = require("./models/person")
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+let Person = new PersonModel();
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
