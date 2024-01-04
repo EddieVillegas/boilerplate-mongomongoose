56821b7104ec313a76b769afe8fe97408650541f
Villegas, Edgar <Edgar.Villegas@mckesson.com>
2024-01-03 20:22:17 -0600

    Find one by food

--- myApp.js
+++ myApp.js
@@ -33,7 +33,7 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  Person.findOne({food}, (error, data) => {
+  Person.findOne({favoriteFoods: food}, (error, data) => {
     if(error) done(error);
     done(null, data);
   })
